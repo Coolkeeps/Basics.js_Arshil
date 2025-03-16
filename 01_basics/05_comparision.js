a770c51458aa0d6244329cd0c463cac48ef0abf0
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 15:38:06 +0000

    05_comparisions-done

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,32 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2==2);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2===2);
+console.log(2!=1);
+
+// some imp comparisions
+
+console.log("2">1);
+console.log("2"<1);
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0 );
+
+// the comparision(> < >= <=) take the null value as 0 
+// sometimes it changes it to NaN
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// for strict check use ===
+console.log("2"===2);
+
+//avoid above type of comparisions
