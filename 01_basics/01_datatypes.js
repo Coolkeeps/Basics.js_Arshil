28805e6671cadc6061d9808ffa6572ea13c3befb
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 08:33:34 +0000

    02_datatypes-done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,31 @@
+// alert(3+3); // we are using node not a browser => error
+
+//readability=>wrong 
+console.log(3
+    +
+    3); console.log("turerewe");
+
+//correct readebility
+console.log(3+3);
+
+console.log("arhsil");
+
+//datatypes:
+
+let name = "arshil" //string
+let age = 16 // number 
+let loggedIn = true //boolean
+let state; // undefined 
+//null => standAlone Value => object
+// symbol => unique
+
+//object
+
+// type of anything can be found out using "typeof" syntax
+
+console.log(typeof name);
+
+
+
+
+
