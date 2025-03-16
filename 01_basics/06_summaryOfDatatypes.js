f4fa460004f1ebe14aba8fdbcb895adaef381332
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 17:28:34 +0000

    06_datatypesSummary-done

---
+++ 01_basics/06_summaryOfDatatypes.js
@@ -0,0 +1,43 @@
+// two types of datatypes {primitive and non-primitive}
+
+//primitive : strings, number , boolean , undefined , null , symbol , bigint
+
+//strings
+let name = "arshil"
+
+//number 
+let age = 16
+console.log();
+
+//boolean
+let isLoggedIn = true
+
+//undefined
+let state;
+
+//null
+let temp = null
+
+//bigint
+let sciNo = 1234323121312412312412312n
+
+//symbol
+let sym = Symbol(1221)
+
+// non-primitive : object, arrays , functions
+
+//array
+const heroes = ["ironman", "thor", "jarvis"]
+
+//objects 
+const myData = {
+    name:"arshil",
+    age:16
+}
+
+// functions
+let MyFunc = function(){
+    console.log("hello wrld");
+}
+
+// all the non-primitive datatypes reutrn their datatypes as =>object functions
