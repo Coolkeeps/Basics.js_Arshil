b22336726302e1d971f93cf472bb1780739dbb34
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 14:32:27 +0000

    03_conversions-done

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,16 @@
+let score = "3312ca"
+console.log(typeof score);
+
+ScoreInString = Number(score)// the string is converted to no but while printing its NAN
+console.log(typeof ScoreInString);
+console.log(ScoreInString); 
+
+// if boolean value is chosen and its true then =>1 else =>0
+// if null is chosen then it returns =>0
+// if undefined is chosen it returns => NAN
+// empty string returns false
+
+
+
+
+
