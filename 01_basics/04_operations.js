77206b3561ddc62dbc3e7bd538f67c2b2e9f30a5
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 15:19:20 +0000

    04_operations-done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,40 @@
+let value = 4
+let negValue = -value
+
+console.log(negValue);
+
+//   simple operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1 = "arshil"
+let str2 = " ayaan"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+str4 = (1+"2");
+str5 = ("1"+2);
+
+// console.log(typeof str4);
+// console.log(typeof str5);
+
+str6 = ("1"+2+3);
+// console.log(str6);
+// console.log(typeof str6);
+
+str7 = (1+1+"3");
+// console.log(str7);
+// console.log(typeof str7)
+
+// console.log(true);
+// console.log(+true);
+
+counter = 100
+counter--;
+console.log(counter);
+
+
