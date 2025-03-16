653d1a24b623dc0f7a2fbe3ae404c1b46c84b38d
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-16 00:35:07 +0000

    01_variables-done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 12345;
+let accountEmail = "arshil@gmail.com";
+var accountPassword = "45678483"
+accountCity = "Agra"
+
+
+// accountId = 4 
+// cannot assign another value to a constant 
+
+accountEmail = "ak@ak.com"
+// the value of "let" can be assigned/changed any time
+
+accountPassword = "234242"
+//the value of "var" can also be assigned/changed any time 
+
+accountCity = "delhi"
+//variables can also be considered without using fixed keywords and they can also be assigned/changed too
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+//never use "var" variable 
+//always use "let" and "const" variables
+
+
