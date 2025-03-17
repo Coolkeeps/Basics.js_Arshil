e6af60ebeed3cf9c2158766ed00e3904d55d9943
Arshil Ayaan <166119491+Coolkeeps@users.noreply.github.com>
2025-03-17 00:15:48 +0000

    08_strings-done

---
+++ 01_basics/08_strings.js
@@ -0,0 +1,25 @@
+let name = "arshil"
+let count = 50 
+
+console.log(name+count+" hehe");
+console.log(`my name is ${name} and my age is ${count}`)
+
+const gameName = new String("a-rshiya-a")
+let  newName = gameName.length
+console.log(newName);
+
+let sepName = gameName.split("-")
+console.log(sepName);
+
+let slicedName = gameName.slice(1,4)
+console.log(slicedName);
+
+let str2 = "arshil    "
+console.log(str2.trim());
+
+console.log(str2.search("s"))
+console.log(str2.replace("s","a"));
+
+console.log(str2.indexOf("l"));
+
+
