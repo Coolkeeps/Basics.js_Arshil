5a0d297a32f09366a2a822062eecbee24a2eb8ba
Arshil <arshilayaan3000@gmail.com>
2025-03-18 15:30:52 +0530

    functions-complete

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,7 +6,7 @@ function sayMyName(){
     console.log("i");
     console.log("l");
 }
-sayMyName()//this is execution
+// sayMyName()//this is execution
 sayMyName//this is reference
 
 // function AddTwoNumbers(number1,number2){
@@ -36,4 +36,62 @@ function loginUserMessage(username = "no one"){
     }
     return`${username} just logged in `
 }
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+
+////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// function calculateCartPrice(num1){
+//     return num1
+//  }
+
+// console.log(calculateCartPrice(2)) 
+// console.log(calculateCartPrice(200,300,400))// this does not contaon all the values
+
+// hence spread and rest operator is used
+
+// function calculateCartPrice(...num1) {
+//     return num1
+// }
+// console.log(calculateCartPrice(100,200,300));
+
+
+function calculateCartPrice (val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(100,200,300,400));
+
+
+const data = {
+    username:"hitch",
+    price: 299
+}
+
+// const {username} = data
+// const {price} = data
+// console.log(username);
+// console.log(price);
+
+
+
+function handleAnyObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleAnyObject(data)
+
+handleAnyObject(
+    {
+        username:"arshil",
+        price:499
+    }
+)
+
+const myArr = [200,100,220,400]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+console.log(returnSecondValue(myArr));
