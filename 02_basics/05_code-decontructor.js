a38780611750ea45db22fa383ef4c0ab634b53d9
Arshil <arshilayaan3000@gmail.com>
2025-03-18 14:17:16 +0530

    05-api-json-done

---
+++ 02_basics/05_code-decontructor.js
@@ -0,0 +1,24 @@
+const course = {
+    courseName : "chai aur code",
+    coursePrice: 999,
+    courseTeacher : "hitesh"
+}
+
+console.log(course.courseTeacher)
+const {courseTeacher:teacher}= course
+console.log(teacher);
+
+// {
+//     "name":"hites",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+//get api froom the url and then use the json formatter
