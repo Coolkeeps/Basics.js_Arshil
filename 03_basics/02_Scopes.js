0a6e7ed0af32a0f06b3c104472e9bd69a9e6eb1b
Arshil <arshilayaan3000@gmail.com>
2025-03-18 23:08:56 +0530

    02_nested_scopes-done

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -20,3 +20,50 @@ if (true){   // all the declaration are made within the inner block scope
 
 
 //the scope over the net is diiferent and scope over coding environment is diff
+
+
+//++++++++++++++++++++++++++nested scopes++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username = "arshil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+    
+}
+
+one()
+
+// the elements of the parent can be accesed by the child but reverse is not possible
+
+if (true){
+    const username ="arjhsil"
+
+    if (true){
+        const website = "youtube"
+        console.log(website + username);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++++++++++++++++++interesting++++++++++++++++++++++++++++
+
+
+addone(5)// here since no variable is initialised and direct declaration is there therefore it can be accesed earlier too
+function addone(num){
+    return num + 1
+}
+
+
+const addtwo = function (num){
+    return num + 2
+}
+
+addtwo(5)// this cannot be returned before the initialisation of the variable
+
